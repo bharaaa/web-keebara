b4dbed63db88390c65ec5982160c195d0448abac
bharaa <bharaa@10.10.102.104>
2024-04-21 23:17:04 +0700

    [IMPROVE] added home

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,11 +1,13 @@
 import { Component } from "react";
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 export default class Home extends Component {
     render() {
         return(
-            <div>
+            <div className="background-container">
                 <Navbar/>
+                <Hero/>
             </div>
         )
     }
