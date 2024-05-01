91e3b4dd0d5496570e5047b2dde17670e03b9f9a
bharaa <bharaa@Bharas-MacBook-Air.local>
2024-05-01 09:48:19 +0700

    [IMPROVE] change app to functional component

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,12 @@ import './App.css'
 import { Component } from 'react'
 import Home from './Home/Home'
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <Home/>
-      </div>
-    )
-  }
+function App() {
+  return (
+    <div>
+      <Home/>
+    </div>
+  )
 }
 
 export default App
