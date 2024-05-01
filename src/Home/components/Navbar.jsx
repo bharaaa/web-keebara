bd6b4115e4198ac5169bec2be9379b575a76e89f
bharaa <bharaa@Bharas-MacBook-Air.local>
2024-05-01 09:50:47 +0700

    [IMPROVE] change navbar to functional component

--- src/Home/components/Navbar.jsx
+++ src/Home/components/Navbar.jsx
@@ -1,32 +1,30 @@
 import { Component } from "react";
 
-export default class Navbar extends Component{
-    render() {
-        return(
-            <>
-            <nav className="navbar navbar-expand-lg sticky-top mb-3">
-                <div className="containe-fluid">
-                    <a className="navbar-brand text-black fs-3 ms-3" href="#">
-                        <b>Keebara</b> Store
-                    </a>    
-                </div>
-                <div>
-                    <button
-                        type="button"
-                        className="btn text-black ms-3"
-                    >
-                        Home
-                    </button>
-                    <button
-                        type="button"
-                        class="btn text-black ms-3"
-                    >
-                        About Us
-                    </button>
-                    
-                </div>
-            </nav>
-            </>
-        )
-    }
+export default function Navbar() {
+    return(
+        <>
+        <nav className="navbar navbar-expand-lg sticky-top mb-3">
+            <div className="containe-fluid">
+                <a className="navbar-brand text-black fs-3 ms-3" href="#">
+                    <b>Keebara</b> Store
+                </a>    
+            </div>
+            <div>
+                <button
+                    type="button"
+                    className="btn text-black ms-3"
+                >
+                    Home
+                </button>
+                <button
+                    type="button"
+                    class="btn text-black ms-3"
+                >
+                    About Us
+                </button>
+                
+            </div>
+        </nav>
+        </>
+    )
 }
