7b6ce835ee182172e52979f86b9d9621726b4742
bharaa <bharaa@Bharas-MacBook-Air.local>
2024-05-01 09:49:44 +0700

    [IMPROVE] change home to functional component

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -2,13 +2,11 @@ import { Component } from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 
-export default class Home extends Component {
-    render() {
-        return(
-            <div className="background-container">
-                <Navbar/>
-                <Hero/>
-            </div>
-        )
-    }
+export default function Home() {
+    return(
+        <div className="background-container">
+            <Navbar/>
+            <Hero/>
+        </div>
+    )
 }
